e34259d36c682fc06bf4b38a93f28a88a2308885
WCharlesOH <williams.oliverah@gmail.com>
2025-11-25 19:42:46 -0500

    Error pequeño

--- Pagina_Streams/src/App.tsx
+++ Pagina_Streams/src/App.tsx
@@ -52,10 +52,8 @@ export default function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/registro" element={<Registro />} />
               <Route path="/nosotros" element={<Nosotros />} />
-              <Route path="/terminos" element={<Terminos />} />
-
-              <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} />} />
-
+              <Route path="/terminos" element={<Terminos />} />   
+              <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} setMonedas={setMonedas} />} />
               <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} setMonedas={setMonedas} />} />
               <Route path="/panel" element={<PanelControl monedas={monedas} setMonedas={setMonedas} />} />
               <Route path="/suscripciones" element={<Suscripciones />} />
