efa9e1b14751388ad8a58eb6dfe9658d2642182a
WCharlesOH <williams.oliverah@gmail.com>
2025-11-25 19:52:27 -0500

    dsadsadad

--- Pagina_Streams/src/App.tsx
+++ Pagina_Streams/src/App.tsx
@@ -53,10 +53,8 @@ export default function App() {
               <Route path="/registro" element={<Registro />} />
               <Route path="/nosotros" element={<Nosotros />} />
               <Route path="/terminos" element={<Terminos />} />
-
               <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} />} />
-
-              <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} setMonedas={setMonedas} />} />
+              <Route path="/dashboard" element={<DashboardStreamer monedas={monedas} />} />
               <Route path="/panel" element={<PanelControl monedas={monedas} setMonedas={setMonedas} />} />
               <Route path="/suscripciones" element={<Suscripciones />} />
               <Route path="/ajustes" element={<Ajustes />} />
