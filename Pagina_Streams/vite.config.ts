e453bd48a73111aa2fd9767041529721c7ebc883
WCharlesOH <williams.oliverah@gmail.com>
2025-12-01 08:43:15 -0500

    changes to the Vite configuration file to set the root directory.

--- Pagina_Streams/vite.config.ts
+++ Pagina_Streams/vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ProyectoPW/'
+  base: '/ProyectoPW/',
+  root: './Pagina_Streams',
 })
